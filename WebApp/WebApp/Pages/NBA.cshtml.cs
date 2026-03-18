b46de0d630f015856cc6fafc15cc90b70141817b
Shawn Conboy <conboysbusiness@gmail.com>
2026-03-18 04:45:09 -0400

    Initialized the project. Added to index page, created nba and nfl razor pages.

---
+++ WebApp/WebApp/Pages/NBA.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages
+{
+    public class NBAModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ WebApp/WebApp/Pages/NFL.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages
+{
+    public class NFLModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
