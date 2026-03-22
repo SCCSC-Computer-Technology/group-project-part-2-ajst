9a437529eb52afbe991945fe035ac14313b79480
Shawn Conboy <conboysbusiness@gmail.com>
2026-03-22 05:58:23 -0400

    Updated players page to filter and route based on selected teams

---
+++ WebApp/WebApp/Data/SportsDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApp.Models;
+
+namespace WebApp.Data
+{
+    public class SportsDbContext : DbContext
+    {
+        public SportsDbContext(DbContextOptions<SportsDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<NBAPlayer> NBAPlayers { get; set; }
+        public DbSet<NBATeam> NBATeams { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<NBAPlayer>().ToTable("nbaCurrentPlayers");
+
+            modelBuilder.Entity<NBAPlayer>().HasKey(p => p.PlayerID);
+        }
+    }
+}
---
+++ WebApp/WebApp/Models/NBAPlayer.cs
@@ -0,0 +1,19 @@
+﻿namespace WebApp.Models
+{
+    public class NBAPlayer
+    {
+        public short PlayerID { get; set; } 
+        public string Name { get; set; }
+        public byte TeamID { get; set; }       
+        public string Position { get; set; }
+        public byte Height { get; set; }       
+        public short Weight { get; set; }   
+        public byte YearsInLeague { get; set; }
+        public short BirthYear { get; set; }
+        public string BornIn { get; set; }
+        public short DraftYear { get; set; }
+        public byte DraftRound { get; set; }   
+        public byte DraftPick { get; set; }    
+        public string HeadshotUrl { get; set; }
+    }
+}
