470d9cfd45e2d7161fa1613a5d048a4a72db116c
JaydeeOrt <jorgedortiz.07@gmail.com>
2026-03-25 01:09:57 -0400

    Data Shows
    Got the data to appear on NFL Teams and NBA Stats

---
+++ WebApp/WebApp/Models/NFLTeam.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApp.Models
+{
+    public class NFLTeam
+    {
+        public int NFLTeamID { get; set; }
+        public string TeamName { get; set; }
+        public string Abbreviation { get; set; }
+        public string Conference {  get; set; }
+        public string Stadium { get; set; }
+        public int Wins { get; set; }
+        public int Losses { get; set; }
+        public int Ties { get; set; }
+
+    }
+}
--- WebApp/WebApp/obj/Debug/net10.0/WebApp.AssemblyInfo.cs
+++ WebApp/WebApp/obj/Debug/net10.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a76b3fc2ed5d9c570133ec7ed9914001d8dcc14e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+095c6a16f7fd623b5c329d39ee785ad200521f57")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
