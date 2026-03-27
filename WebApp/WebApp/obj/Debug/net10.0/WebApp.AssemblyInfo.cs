8a2c1813c33fc51cc853a1d52c3b4821b4ce512d
Trevor Ball <tmball47@sccsc.edu>
2026-03-26 23:40:52 -0400

    Ads added to schedule tabs
    So i was messing around and added ads to the schedule tabs to show epic games and rocket league and even made pages for when the ad buttons are clicked

--- WebApp/WebApp/obj/Debug/net10.0/WebApp.AssemblyInfo.cs
+++ WebApp/WebApp/obj/Debug/net10.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+095c6a16f7fd623b5c329d39ee785ad200521f57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+064d8419885c8b275bb688a81c855369f8ef15bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
